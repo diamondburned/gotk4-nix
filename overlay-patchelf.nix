b365da916b06eec8b3ab18959b762230c0232df0
diamondburned <diamond@libdb.so>
2024-03-22 00:46:35 -0700

    Clear RPATH for patchelf
    See https://github.com/diamondburned/dissent/issues/255.

--- overlay-patchelf.nix
+++ overlay-patchelf.nix
@@ -1,7 +1,12 @@
 self: super: {
 	patchelfer = arch: interpreter: super.writeShellScriptBin
 		"patchelf-${arch}"
-		"${super.patchelf}/bin/patchelf --set-interpreter ${interpreter} \"$@\"";
+		''
+			${super.patchelf}/bin/patchelf \
+				--set-interpreter ${interpreter} \
+				--set-rpath "" \ # clear RPATH, see https://github.com/diamondburned/dissent/issues/255
+				"$@"
+		'';
 	# See https://sourceware.org/glibc/wiki/ABIList.
 	patchelf-x86_64  = self.patchelfer "x86_64"  "/lib64/ld-linux-x86-64.so.2";
 	patchelf-aarch64 = self.patchelfer "aarch64" "/lib/ld-linux-aarch64.so.1";
