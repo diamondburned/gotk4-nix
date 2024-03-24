64647707fef1160bf317a7b18b0632968b36d506
diamondburned <diamond@libdb.so>
2024-03-23 18:45:34 -0700

    Revert "Clear RPATH for patchelf"
    This reverts commit b365da916b06eec8b3ab18959b762230c0232df0.

--- overlay-patchelf.nix
+++ overlay-patchelf.nix
@@ -1,12 +1,7 @@
 self: super: {
 	patchelfer = arch: interpreter: super.writeShellScriptBin
 		"patchelf-${arch}"
-		''
-			${super.patchelf}/bin/patchelf \
-				--set-interpreter ${interpreter} \
-				--set-rpath "" \ # clear RPATH, see https://github.com/diamondburned/dissent/issues/255
-				"$@"
-		'';
+		"${super.patchelf}/bin/patchelf --set-interpreter ${interpreter} \"$@\"";
 	# See https://sourceware.org/glibc/wiki/ABIList.
 	patchelf-x86_64  = self.patchelfer "x86_64"  "/lib64/ld-linux-x86-64.so.2";
 	patchelf-aarch64 = self.patchelfer "aarch64" "/lib/ld-linux-aarch64.so.1";
