20cc81d5185b585c31686f55cba6a30de52cf58d
diamondburned <diamond@libdb.so>
2024-03-31 00:25:05 -0700

    Add flake-compat

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 		flake-utils.url = "github:numtide/flake-utils";
+		flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
 		gomod2nix.url = "github:nix-community/gomod2nix";
 		gomod2nix.inputs.flake-utils.follows = "flake-utils";
   };
