6364463814bf9040f3c8df73858d7aa08e0f2eec
diamondburned <diamond@libdb.so>
2025-02-21 20:47:51 -0800

    Expose go in mkShell

--- build-shell.nix
+++ build-shell.nix
@@ -1,152 +1,172 @@
 self:
 
 {
-	base ? { pname = "gotk4-dev-shell"; },
-	pkgs,
-	shellHook ? "",
-	buildInputs ? [],
-	clangdPackages ? (pkgs: with pkgs; [ gtk4 glib ]),
-	...
+  base ? {
+    pname = "gotk4-dev-shell";
+  },
+  pkgs,
+  shellHook ? "",
+  buildInputs ? [ ],
+  clangdPackages ? (
+    pkgs: with pkgs; [
+      gtk4
+      glib
+    ]
+  ),
+  go ? pkgs.go,
+  ...
 }@args':
 
 let
-	pkgs' = pkgs;
+  pkgs' = pkgs;
 in
 
 let
-	pkgs = pkgs'.extend self.overlays.patchelf;
+  pkgs = pkgs'.extend self.overlays.patchelf;
 
-	inherit (pkgs.extend self.overlays.patchelf)
-		patchelf-x86_64
-		patchelf-aarch64;
+  inherit (pkgs.extend self.overlays.patchelf)
+    patchelf-x86_64
+    patchelf-aarch64
+    ;
 
-	lib = pkgs.lib;
+  lib = pkgs.lib;
 
-	args = builtins.removeAttrs args' [
-		"base"
-		"pkgs"
-		"shellHook"
-		"buildInputs"
-		"clangdPackages"
-	];
+  args = builtins.removeAttrs args' [
+    "base"
+    "pkgs"
+    "shellHook"
+    "buildInputs"
+    "clangdPackages"
+  ];
 
-	baseBuildInputs = base.buildInputs or (_: []);
-	baseNativeBuildInputs = base.nativeBuildInputs or (_: []);
+  baseBuildInputs = base.buildInputs or (_: [ ]);
+  baseNativeBuildInputs = base.nativeBuildInputs or (_: [ ]);
 
-	baseDependencies = with pkgs; [
-		# Bare minimum required.
-		atk
-		gtk3
-		gtk4
-		glib
-		graphene
-		gdk-pixbuf
-		gobject-introspection
-		librsvg
-	];
+  baseDependencies = with pkgs; [
+    # Bare minimum required.
+    atk
+    gtk3
+    gtk4
+    glib
+    graphene
+    gdk-pixbuf
+    gobject-introspection
+    librsvg
+  ];
 
-	buildInputs' = lib.unique ((with pkgs; [
-		pkg-config
+  buildInputs' = lib.unique (
+    (with pkgs; [
+      pkg-config
 
-		go
-		gopls
-		gotools
+      gopls
+      gotools
 
-		clangd
-		clang-tools # for clang-format
+      clangd
+      clang-tools # for clang-format
 
-		git
-		patchelf-x86_64-linux
-		patchelf-aarch64-linux
-	])
-	++ (baseDependencies)
-	++ (buildInputs)
-	++ (baseBuildInputs pkgs)
-	++ (baseNativeBuildInputs pkgs));
+      git
+      patchelf-x86_64-linux
+      patchelf-aarch64-linux
+    ])
+    ++ [ go ]
+    ++ (baseDependencies)
+    ++ (buildInputs)
+    ++ (baseBuildInputs pkgs)
+    ++ (baseNativeBuildInputs pkgs)
+  );
 
-	# Workaround for the lack of wrapGAppsHook:
-	# https://nixos.wiki/wiki/Development_environment_with_nix-shell
-	gotk4-env =  pkgs.runCommandLocal "gotk4-env" {
-		buildInputs = buildInputs';
-		nativeBuildInputs = buildInputs' ++ (with pkgs; [ wrapGAppsHook ]);
-	} ''
-		cat<<'EOF' > ./dump
-#!${pkgs.runtimeShell}
-envs=(
-	XDG_DATA_DIRS
-	GSETTINGS_SCHEMAS_PATH
+  # Workaround for the lack of wrapGAppsHook:
+  # https://nixos.wiki/wiki/Development_environment_with_nix-shell
+  gotk4-env =
+    pkgs.runCommandLocal "gotk4-env"
+      {
+        buildInputs = buildInputs';
+        nativeBuildInputs = buildInputs' ++ (with pkgs; [ wrapGAppsHook ]);
+      }
+      ''
+        cat<<-'EOF' > ./dump
+        #!${pkgs.runtimeShell}
+        envs=(
+          XDG_DATA_DIRS
+          GSETTINGS_SCHEMAS_PATH
+        )
+        for env in "''${envs[@]}"; {
+          printf "%s %q\n" "$env" "''${!env}"
+        }
+        EOF
+        chmod +x ./dump
+
+        gappsWrapperArgsHook
+        wrapGApp ./dump
+
+        ./dump > $out
+      '';
+
+  clangd = pkgs.writeShellScriptBin "clangd" (
+    with pkgs.gnome;
+    with pkgs;
+    with lib;
+    ''
+      for path in ${concatStringsSep " " (map (pkg: pkg.dev.outPath) (clangdPackages pkgs))}; do
+        for file in $path/lib/pkgconfig/*.pc; do
+          [[ ! -f $file ]] && continue
+          packages+=( $(basename $file .pc) )
+        done
+      done
+
+      for pkg in ''${packages[@]}; do
+        cpath=$(pkg-config $pkg --cflags-only-I | sed 's/ -I/:/g' | sed 's/^-I//')
+        if [[ $cpath == "" ]]; then
+          continue
+        fi
+
+        if [[ -z $CPATH ]]; then
+          export CPATH=$cpath
+        else
+          export CPATH=$CPATH:$cpath
+        fi
+      done
+
+      exec ${pkgs.clang-tools}/bin/clangd "$@"
+    ''
+  );
+
+in
+pkgs.mkShell (
+  {
+    name = "${base.pname}-nix-shell";
+
+    buildInputs = buildInputs';
+
+    shellHook =
+      with pkgs.gnome;
+      with pkgs;
+      ''
+        while read -r key value; do
+          export $key=''${!key}''${!key:+:}$value
+        done < ${gotk4-env}
+
+        export XDG_DATA_DIRS=$XDG_DATA_DIRS:${hicolor-icon-theme}/share:${adwaita-icon-theme}/share
+        export XDG_DATA_DIRS=$XDG_DATA_DIRS:$GSETTINGS_SCHEMAS_PATH
+
+        ${shellHook}
+      '';
+
+    # For debugging stack traces.
+    NIX_DEBUG_INFO_DIRS = lib.makeSearchPath "lib/debug" (
+      map (pkg: if pkg ? "debug" then pkg.debug else pkg) baseDependencies
+    );
+
+    CGO_ENABLED = "1";
+
+    # Use /tmp, since /run/user/1000 (XDG_RUNTIME_DIRECTORY) might be too small.
+    # See https://github.com/NixOS/nix/issues/395.
+    TMP = "/tmp";
+    TMPDIR = "/tmp";
+
+    passthru = {
+      inherit baseDependencies;
+    };
+  }
+  // args
 )
-for env in "''${envs[@]}"; {
-	printf "%s %q\n" "$env" "''${!env}"
-}
-EOF
-		chmod +x ./dump
-
-		gappsWrapperArgsHook
-		wrapGApp ./dump
-
-		./dump > $out
-	'';
-
-	clangd = pkgs.writeShellScriptBin "clangd" (
-		with pkgs.gnome;
-		with pkgs;
-		with lib;
-		''
-			for path in ${concatStringsSep " " (map (pkg: pkg.dev.outPath) (clangdPackages pkgs))}; do
-				for file in $path/lib/pkgconfig/*.pc; do
-					[[ ! -f $file ]] && continue
-					packages+=( $(basename $file .pc) )
-				done
-			done
-
-			for pkg in ''${packages[@]}; do
-				cpath=$(pkg-config $pkg --cflags-only-I | sed 's/ -I/:/g' | sed 's/^-I//')
-				if [[ $cpath == "" ]]; then
-					continue
-				fi
-
-				if [[ -z $CPATH ]]; then
-					export CPATH=$cpath
-				else
-					export CPATH=$CPATH:$cpath
-				fi
-			done
-
-			exec ${pkgs.clang-tools}/bin/clangd "$@"
-		'');
-
-in pkgs.mkShell ({
-	name = "${base.pname}-nix-shell";
-
-	buildInputs = buildInputs';
-
-	shellHook = with pkgs.gnome; with pkgs; ''
-		while read -r key value; do
-			export $key=''${!key}''${!key:+:}$value
-		done < ${gotk4-env}
-
-		export XDG_DATA_DIRS=$XDG_DATA_DIRS:${hicolor-icon-theme}/share:${adwaita-icon-theme}/share
-		export XDG_DATA_DIRS=$XDG_DATA_DIRS:$GSETTINGS_SCHEMAS_PATH
-
-		${shellHook}
-	'';
-
-	# For debugging stack traces.
-	NIX_DEBUG_INFO_DIRS = lib.makeSearchPath
-		"lib/debug"
-		(map
-			(pkg: if pkg ? "debug" then pkg.debug else pkg)
-			baseDependencies);
-
-	CGO_ENABLED  = "1";
-
-	# Use /tmp, since /run/user/1000 (XDG_RUNTIME_DIRECTORY) might be too small.
-	# See https://github.com/NixOS/nix/issues/395.
-	TMP    = "/tmp";
-	TMPDIR = "/tmp";
-
-	passthru = {
-		inherit baseDependencies;
-	};
-} // args)
--- flake.nix
+++ flake.nix
@@ -2,48 +2,59 @@
   description = "gotk4 Nix utility flake";
 
   inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-		flake-utils.url = "github:numtide/flake-utils";
-		flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
-		gomod2nix.url = "github:nix-community/gomod2nix";
-		gomod2nix.inputs.flake-utils.follows = "flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    gomod2nix.inputs.flake-utils.follows = "flake-utils";
   };
 
-	outputs = { self, nixpkgs, flake-utils, ... }: {
-		lib = {
-			mkShell = import ./build-shell.nix self;
-			mkSource = import ./build-source.nix self;
-			mkPackage = import ./build-package.nix self;
-			mkPackageCross = import ./build-cross.nix self;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    {
+      lib = {
+        mkShell = import ./build-shell.nix self;
+        mkSource = import ./build-source.nix self;
+        mkPackage = import ./build-package.nix self;
+        mkPackageCross = import ./build-cross.nix self;
 
-			mkLib = { base, pkgs }: nixpkgs.lib.mapAttrs
-				(name: fn: { ... }@args: fn (args // { inherit base pkgs; }))
-				{
-					inherit (self.lib)
-						mkShell
-						mkSource
-						mkPackage
-						mkPackageCross;
-				};
-		};
+        mkLib =
+          { base, pkgs }:
+          nixpkgs.lib.mapAttrs (name: fn: { ... }@args: fn (args // { inherit base pkgs; })) {
+            inherit (self.lib)
+              mkShell
+              mkSource
+              mkPackage
+              mkPackageCross
+              ;
+          };
+      };
 
-		overlays = {
-			patchelf = import ./overlay-patchelf.nix;
-			patchedGo = import ./overlay-patched-go.nix;
-		};
-	} // (flake-utils.lib.eachDefaultSystem (system:
-		let
-			pkgs = nixpkgs.legacyPackages.${system};
-		in
-		{
-			packages = {
-				upload-artifacts = import ./upload-artifacts.nix {
-					inherit pkgs;
-				};
-				inherit (pkgs.extend self.overlays.patchelf)
-					patchelf-x86_64-linux
-					patchelf-aarch64-linux;
-			};
-		}
-	));
+      overlays = {
+        patchelf = import ./overlay-patchelf.nix;
+        patchedGo = import ./overlay-patched-go.nix;
+      };
+    }
+    // (flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          upload-artifacts = import ./upload-artifacts.nix {
+            inherit pkgs;
+          };
+          inherit (pkgs.extend self.overlays.patchelf)
+            patchelf-x86_64-linux
+            patchelf-aarch64-linux
+            ;
+        };
+      }
+    ));
 }
